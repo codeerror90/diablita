0b39517e06c07c6cf53eb3fadd06afb726a8fc2d
codeerror90 <79945630+codeerror90@users.noreply.github.com>
2021-12-13 15:39:50 -0600

    Update shirobot.js

--- shirobot.js
+++ shirobot.js
@@ -9043,7 +9043,7 @@ Fg.sendMessage(from, aing, text, {quoted: mek, contextInfo: {"mentionedJid": [se
 		console.log('\x1b[1;31m', color("➛ ", "red"), color("Estado: "), color("Chat privado", "red"))
 		console.log('\x1b[1;31m', color("➛ ", "red"), color("De: "), color(`${sender.split("@")[0]}`, "orange"))
       reply('❎ Esta prohibido hablar al privado del bot, seras bloqueado automáticamente')
-      Fg.sendMessage("51940617554-1600359399@g.us", `✳️ Hola *@${sender.split("@")[0]}* está prohibido hablar al pv del bot, podrías ser eliminado del grupo`, text, {contextInfo: {"mentionedJid": [sender]}})
+      Fg.sendMessage("120363022067597077@g.us", `✳️ Hola *@${sender.split("@")[0]}* está prohibido hablar al pv del bot, podrías ser eliminado del grupo`, text, {contextInfo: {"mentionedJid": [sender]}})
       setTimeout( () => {
       	Fg.blockUser(sender, 'add') 
                   }, 1000)
